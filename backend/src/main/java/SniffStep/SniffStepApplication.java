b5629e2a98e932a8b4cc9f2ea32cb5018c99805b
minthug <jkisimmortal4@gmail.com>
2024-04-03 15:00:03 +0900

    Add initial files for Gradle and Spring Boot Application

---
+++ backend/src/main/java/SniffStep/SniffStepApplication.java
@@ -0,0 +1,13 @@
+package SniffStep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SniffStepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SniffStepApplication.class, args);
+	}
+
+}
