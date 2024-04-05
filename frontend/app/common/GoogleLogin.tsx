93fb675553eb2a6625348baac1c6539432ebcdd7
benny <vvsogi@gmail.com>
2024-04-05 18:56:06 +0900

    Refactor common components

--- frontend/app/common/GoogleLogin.tsx
+++ frontend/app/common/GoogleLogin.tsx
@@ -4,9 +4,10 @@ import { D2CodingBold } from '../fonts'
 interface Props {
     theme?: 'light' | 'dark'
     children: React.ReactNode
+    className?: string
 }
 
-export function GoogleLogin({ theme = 'light', children }: Props) {
+export function GoogleLogin({ theme = 'light', children, className }: Props) {
     return (
         <button
             style={{
@@ -14,7 +15,10 @@ export function GoogleLogin({ theme = 'light', children }: Props) {
                 color: theme === 'light' ? '#000' : '#fff',
                 border: theme === 'light' ? '1px solid #d1d5db' : 'none'
             }}
-            className={`${D2CodingBold.className} w-full flex gap-4 justify-center items-center mb-4 p-4 rounded-[100px] select-none shadow-sm`}
+            className={`
+                ${D2CodingBold.className} w-full flex gap-4 justify-center items-center mb-4 p-4 rounded-[100px] select-none shadow-sm
+                ${className}
+            `}
         >
             <img className="w-[18px] h-[18px] object-cover" src="/googleIcon.webp" alt="" />
             {children}
--- frontend/app/common/SigninInput.tsx
+++ frontend/app/common/SigninInput.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
 interface Props {
+    value: string
     placeholder: string
     type: 'text' | 'password'
+    className?: string
     onChange: (value: string) => void
 }
 
-export default function SigninInput({ placeholder, type, onChange }: Props) {
+export default function SigninInput({ value, placeholder, type, className, onChange }: Props) {
     return (
         <input
-            className="w-full p-4 outline-none border border-gray-300 rounded-2xl text-[14px]"
+            className={`w-full p-4 outline-none border border-gray-300 rounded-2xl text-[14px] ${className}`}
+            value={value}
             placeholder={placeholder}
             type={type}
             onChange={(e) => onChange(e.target.value)}
