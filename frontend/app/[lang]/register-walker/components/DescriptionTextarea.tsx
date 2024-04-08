7510debb399685f94061fddf98c419016c17f346
benny <vvsogi@gmail.com>
2024-04-08 17:44:40 +0900

    Add new components for choosing image file, walk dates, walk times, and description modal

---
+++ frontend/app/[lang]/register-walker/components/DescriptionTextarea.tsx
@@ -0,0 +1,26 @@
+import { MAX_DESCRIPTION_SIZE } from '@/app/hooks/useRegisterWalker'
+import React from 'react'
+import ReactTextareaAutosize from 'react-textarea-autosize'
+
+interface Props {
+    description: string
+    descriptionExample: string
+    descriptionSizeError: boolean
+    handleDescriptionChange: (description: string) => void
+}
+
+export default function DescriptionTextarea({ description, descriptionExample, descriptionSizeError, handleDescriptionChange }: Props) {
+    return (
+        <>
+            <ReactTextareaAutosize
+                className="w-full p-4 border rounded-md resize-none outline-none"
+                minRows={10}
+                maxRows={50}
+                value={description}
+                placeholder={descriptionExample}
+                onChange={(e) => handleDescriptionChange(e.target.value)}
+            />
+            {descriptionSizeError && <div className="text-[12px] text-[#ff0000]">최대 {MAX_DESCRIPTION_SIZE}자까지 입력 가능합니다.</div>}
+        </>
+    )
+}
