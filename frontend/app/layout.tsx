0413bfb6a95d8188f9b60f4c2cd37c7df750533c
benny <vvsogi@gmail.com>
2024-04-10 17:59:51 +0900

    Add favicon links to layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <link rel="icon" href="/favicons/favicon-96x96.png" sizes="any" />
             <body className={`${inter.className} bg-[#fcfcfc]`}>{children}</body>
         </html>
     )
