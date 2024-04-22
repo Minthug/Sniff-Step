b8c6add24495dff20ed2d7629c8f18372ea618ad
benny <vvsogi@gmail.com>
2024-04-22 16:28:27 +0900

    Refactor layout components to use localized header and footer text

--- frontend/app/[lang]/boards/layout.tsx
+++ frontend/app/[lang]/boards/layout.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { Locales } from '@/app/types/locales'
-import { Header, container } from '@/app/common'
-import { BannerDesktop, BannerMobile } from './components'
-import { Footer } from '@/app/common/footer/Footer'
+import { LocaleFooter, LocaleHeader, Locales } from '@/app/types/locales'
+import { Footer, Header } from '@/app/common'
+import { getLocales } from '@/app/utils/getLocales'
 
 interface Props {
     children: React.ReactNode
     params: { lang: Locales }
 }
 
-export default function page({ children, params: { lang } }: Props) {
+export default async function page({ children, params: { lang } }: Props) {
+    const headerText = await getLocales<LocaleHeader>('header', lang)
+    const footerText = await getLocales<LocaleFooter>('footer', lang)
+
     return (
         <div>
-            <Header lang={lang} />
+            <Header lang={lang} text={headerText} />
             {children}
-            <Footer lang={lang} />
+            <Footer lang={lang} text={footerText} />
         </div>
     )
 }
--- frontend/app/[lang]/register-walker/layout.tsx
+++ frontend/app/[lang]/register-walker/layout.tsx
@@ -1,19 +1,22 @@
-import { Header, container } from '@/app/common'
-import { Footer } from '@/app/common/footer/Footer'
-import { Locales } from '@/app/types/locales'
 import React from 'react'
+import { Footer, Header, container } from '@/app/common'
+import { LocaleFooter, LocaleHeader, Locales } from '@/app/types/locales'
+import { getLocales } from '@/app/utils/getLocales'
 
 interface Props {
     children: React.ReactNode
     params: { lang: Locales }
 }
 
-export default function page({ children, params: { lang } }: Props) {
+export default async function page({ children, params: { lang } }: Props) {
+    const headerText = await getLocales<LocaleHeader>('header', lang)
+    const footerText = await getLocales<LocaleFooter>('footer', lang)
+
     return (
         <div>
-            <Header lang={lang} />
+            <Header lang={lang} text={headerText} />
             <section className={container.section}>{children}</section>
-            <Footer lang={lang} />
+            <Footer lang={lang} text={footerText} />
         </div>
     )
 }
