499227ebf4d0f6a0d710d92b92173ef62c5e26f8
minthug <jkisimmortal4@gmail.com>
2024-05-11 17:13:29 +0900

    Add password utility and JWT authentication filter

---
+++ backend/src/main/java/SniffStep/common/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -0,0 +1,105 @@
+package SniffStep.common.jwt.filter;
+
+import SniffStep.common.jwt.JwtService;
+import SniffStep.common.utils.PasswordUtil;
+import SniffStep.entity.Member;
+import SniffStep.repository.MemberRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
+
+    private static final String NO_CHECK_URL = "/login";
+
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
+
+    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if (request.getRequestURI().equals(NO_CHECK_URL)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String refreshToken = jwtService.extractRefreshToken(request)
+                .filter(jwtService::isTokenValid)
+                .orElse(null);
+
+        if (refreshToken != null) {
+            checkRefreshTokenAndReIssueAccessToken(response, refreshToken);
+            return;
+        }
+
+        if (refreshToken == null) {
+            checkAccessTokenAndAuthentication(request, response, filterChain);
+        }
+    }
+
+    public void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken) {
+        memberRepository.findByRefreshToken(refreshToken)
+                .ifPresent(member -> {
+                    String reIssueRefreshToken = reIssueRefreshToken(member);
+                    jwtService.sendAccessAndRefreshToken(response, jwtService.createAccessToken(member.getEmail()),
+                    reIssueRefreshToken);
+                });
+    }
+
+    private String reIssueRefreshToken(Member member) {
+        String reIssueRefreshToken = jwtService.createRefreshToken();
+        member.updateRefreshToken(reIssueRefreshToken);
+        memberRepository.saveAndFlush(member);
+        return reIssueRefreshToken;
+    }
+
+    private void checkAccessTokenAndAuthentication(HttpServletRequest request, 
+                                                   HttpServletResponse response, 
+                                                   FilterChain filterChain) throws ServletException, IOException {
+        log.info("checkAccessTokenAndAuthentication() call");
+        jwtService.extractAccessToken(request)
+                .filter(jwtService::isTokenValid)
+                .ifPresent(accessToken -> jwtService.extractEmail(accessToken)
+                        .ifPresent(email -> memberRepository.findByEmail(email)
+                                .ifPresent(this::saveAuthentication)));
+                    
+        filterChain.doFilter(request, response);
+    }
+
+    private void saveAuthentication(Member myMember) {
+        String password = myMember.getPassword();
+        if (password == null) { // 소셜 로그인 유저의 비밀번호 임의 설정하여 소셜 로구ㅡ인 유저도 인증 되도록 설정
+            password = PasswordUtil.generateRandomPassword();
+        }
+
+        UserDetails userDetailsUser = org.springframework.security.core.userdetails.User.builder()
+                .username(myMember.getEmail())
+                .password(password)
+                .roles(myMember.getRole().name())
+                .build();
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(userDetailsUser, null,
+                        authoritiesMapper.mapAuthorities(userDetailsUser.getAuthorities()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
