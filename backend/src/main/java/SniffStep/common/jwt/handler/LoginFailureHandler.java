1d9c9dd7afdf4a5d1b403b52ba8d82811a008cae
minthug <jkisimmortal4@gmail.com>
2024-05-11 18:24:59 +0900

    Refactor OAuth2 user authentication and add failure handler

---
+++ backend/src/main/java/SniffStep/common/jwt/handler/LoginFailureHandler.java
@@ -0,0 +1,23 @@
+package SniffStep.common.jwt.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain;charset=UTF-8");
+        response.getWriter().write("로그인 실패! 이메일 또는 비밀번호를 확인해주세요.");
+        log.info("Login Failure. meg : {}", exception.getMessage());
+    }
+}
