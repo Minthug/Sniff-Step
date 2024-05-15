df831d5483aab60534c27bfcc9c5eca524be4953
benny <vvsogi@gmail.com>
2024-05-15 19:12:20 +0900

    Remove console.log statement in refresh route

--- frontend/app/api/auth/refresh/[userId]/route.ts
+++ frontend/app/api/auth/refresh/[userId]/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server'
 
 export async function GET(req: Request, { params: { userId } }: { params: { userId: string } }) {
-    console.log(userId)
-
     const res = await fetch(process.env.NODE_BACKEND_URL + `/auth/refresh/${userId}`, {
         method: 'GET',
         headers: {
