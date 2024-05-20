370cc11b660cb300d34380314c8768300fefee40
benny <vvsogi@gmail.com>
2024-05-20 18:30:27 +0900

    Add API route for updating boards

---
+++ frontend/app/api/boards/update/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+
+export async function POST(req: Request) {
+    const formdata = await req.formData()
+    const res = await fetch(process.env.NODE_BACKEND_URL + '/boards/update', {
+        method: 'POST',
+        headers: {
+            authorization: req.headers.get('authorization') || ''
+        },
+        body: formdata
+    })
+
+    if (!res.ok) {
+        const { message, error, statusCode } = await res.json()
+        return NextResponse.json({ message, error }, { status: statusCode })
+    }
+
+    return NextResponse.json({ message: 'success' }, { status: 200 })
+}
