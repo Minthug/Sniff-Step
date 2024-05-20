e82ba6e23a814dcbf3041fb4a6cae2304339ad2b
benny <vvsogi@gmail.com>
2024-05-20 17:25:16 +0900

    Add GET route for fetching board data

--- frontend/app/api/boards/[id]/route.ts
+++ frontend/app/api/boards/[id]/route.ts
@@ -1,5 +1,23 @@
 import { NextResponse } from 'next/server'
 
+export async function GET(req: Request, { params: { id } }: { params: { id: string } }) {
+    const res = await fetch(process.env.NODE_BACKEND_URL + `/boards/${id}`, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            authorization: req.headers.get('authorization') || ''
+        }
+    })
+
+    if (!res.ok) {
+        const { message, error, statusCode } = await res.json()
+        return NextResponse.json({ message, error }, { status: statusCode })
+    }
+
+    const { data } = await res.json()
+    return NextResponse.json({ data }, { status: 200 })
+}
+
 export async function DELETE(req: Request, { params: { id } }: { params: { id: string } }) {
     const res = await fetch(process.env.NODE_BACKEND_URL + `/boards/${id}`, {
         method: 'DELETE',
