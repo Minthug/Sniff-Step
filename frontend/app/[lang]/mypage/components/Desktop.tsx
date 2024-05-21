2ac2dc261ecc7f9c83f3e687066f44a96eb0f32a
benny <vvsogi@gmail.com>
2024-05-21 18:51:49 +0900

    Update profile image size in Desktop and Mobile components

--- frontend/app/[lang]/mypage/components/Desktop.tsx
+++ frontend/app/[lang]/mypage/components/Desktop.tsx
@@ -10,7 +10,7 @@ export function Desktop({ profile }: Props) {
     return (
         <div className={container.main.desktop}>
             <div className="h-[250px] flex flex-col justify-center items-center gap-4 my-8 select-none">
-                <img className="min-w-[180px] min-h-[180px] object-contain border rounded-full" src={profile?.profileImage || ''} alt="" />
+                <img className="min-w-[90px] min-h-[90px] object-contain border rounded-full" src={profile?.profileImage || ''} alt="" />
                 <div className="h-[57px]">
                     <div className="text-[24px] text-center">{profile?.nickname}</div>
                     <div className="text-[14px] text-gray-600 text-center">{profile?.email}</div>
--- frontend/app/[lang]/mypage/components/Mobile.tsx
+++ frontend/app/[lang]/mypage/components/Mobile.tsx
@@ -10,7 +10,7 @@ export function Mobile({ profile }: Props) {
     return (
         <div className={container.main.mobile}>
             <div className="h-[500px] flex flex-col justify-center items-center gap-4 my-8 select-none">
-                <img className="min-w-[180px] min-h-[180px] object-contain border rounded-full" src={profile?.profileImage || ''} alt="" />
+                <img className="min-w-[80px] min-h-[80px] object-contain border rounded-full" src={profile?.profileImage || ''} alt="" />
                 <div className="h-[57px]">
                     <div className="text-[24px] text-center">{profile?.nickname}</div>
                     <div className="text-[14px] text-gray-600 text-center">{profile?.email}</div>
