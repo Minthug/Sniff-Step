a140d6abbbfc56dd342144ff7eea206f81ef4284
minthug <jkisimmortal4@gmail.com>
2024-12-31 16:07:54 +0900

    Add validation logic for Notification entity

---
+++ backend/src/main/java/SniffStep/common/exception/InvalidNotificationException.java
@@ -0,0 +1,8 @@
+package SniffStep.common.exception;
+
+public class InvalidNotificationException extends RuntimeException {
+
+    public InvalidNotificationException(String message) {
+        super(message);
+    }
+}
--- backend/src/main/java/SniffStep/entity/Notification.java
+++ backend/src/main/java/SniffStep/entity/Notification.java
@@ -1,9 +1,13 @@
 package SniffStep.entity;
 
 import SniffStep.common.BaseTime;
+import SniffStep.common.exception.InvalidNotificationException;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -14,7 +18,26 @@ public class Notification extends BaseTime {
 
     private String title;
     private String content;
-    private NotficationType notficationType;
+    private NotificationType notificationType;
     private Long memberId;
 
+    @Builder
+    public Notification(String title, String content, NotificationType notificationType, Long memberId) {
+        this.title = title;
+        this.content = content;
+        this.notificationType = notificationType;
+        this.memberId = memberId;
+    }
+
+    private void validateContent(String content) {
+        if (Objects.nonNull(content) && content.length() > CONTENT_LENGTH) {
+            throw new InvalidNotificationException("내용은 50글자 이하이어야 합니다");
+        }
+    }
+
+    private void validateTitle(String title) {
+        if (Objects.nonNull(title) && content.length() > TITLE_LENGTH) {
+            throw new InvalidNotificationException("제목의 길이는 20자 이하이어야 합니다");
+        }
+    }
 }
