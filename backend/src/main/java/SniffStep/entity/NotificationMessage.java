562c87811cb122ffb4ab780d611128a5afa69a2c
minthug <jkisimmortal4@gmail.com>
2025-01-02 16:13:50 +0900

    Add NotificationMessage enum for notification handling

---
+++ backend/src/main/java/SniffStep/entity/NotificationMessage.java
@@ -0,0 +1,22 @@
+package SniffStep.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.text.MessageFormat;
+import java.util.Objects;
+
+@Getter
+@RequiredArgsConstructor
+public enum NotificationMessage {
+
+    NEW_EVENT("뉴 이벤트"),
+    DEADLINE("마감 임박");
+
+    private final String title;
+    private final String contentFormat;
+
+    public String getContentFromFormat(Object... arguments) {
+        return MessageFormat.format(contentFormat, arguments);
+    }
+}
