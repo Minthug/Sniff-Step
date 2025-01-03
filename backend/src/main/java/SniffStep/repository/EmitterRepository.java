4948bc092ac1e2bafd236402c129c9f5d9ac5a3e
minthug <jkisimmortal4@gmail.com>
2025-01-03 16:42:41 +0900

    Implement SSE notification functionality with reconnecting support

--- backend/src/main/java/SniffStep/repository/EmitterRepository.java
+++ backend/src/main/java/SniffStep/repository/EmitterRepository.java
@@ -2,10 +2,13 @@ package SniffStep.repository;
 
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.util.Map;
+
 public interface EmitterRepository {
 
     void save(String emitterId, SseEmitter sseEmitter);
 
     void deleteById(String emitterId);
 
+    Map<String, SseEmitter> findAllByIdStartWith(Long memberId);
 }
--- backend/src/main/java/SniffStep/service/NotificationService.java
+++ backend/src/main/java/SniffStep/service/NotificationService.java
@@ -3,10 +3,16 @@ package SniffStep.service;
 import SniffStep.repository.EmitterRepository;
 import SniffStep.repository.MemberRepository;
 import SniffStep.service.request.ConnectNotificationCommand;
+import SniffStep.service.response.NotificationResponse;
+import io.jsonwebtoken.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+
+import static java.lang.String.format;
 
 @Slf4j
 @Service
@@ -20,7 +26,61 @@ public class NotificationService {
 
 
     public SseEmitter connectNotification(ConnectNotificationCommand command) {
-        Long memberId = command.memberId();
-        return emitter;
+        Long memberId = command.getMemberId();
+        String lastEventId = command.getLastEventId();
+
+        String emitterId = format("{0}_{1}", memberId, System.currentTimeMillis());
+        SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
+        emitterRepository.save(emitterId, sseEmitter);
+
+        sseEmitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
+        sseEmitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
+        sseEmitter.onError(e -> emitterRepository.deleteById(emitterId));
+
+
+        send(sseEmitter, emitterId, format("[connected] MemberId={0}", memberId));
+
+        if (!command.getLastEventId().isEmpty()) {
+            Map<String, SseEmitter> events = emitterRepository.findAllByIdStartWith(memberId);
+            events.entrySet().stream().filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
+                    .forEach(entry -> send(sseEmitter, emitterId, entry.getValue()));
+        }
+        return sseEmitter;
+    }
+
+    /**
+     * 알림 객체 전송용
+     * @param emitter
+     * @param emitterId
+     * @param data
+     */
+    private void send(SseEmitter emitter, String emitterId, NotificationResponse data) {
+        try {
+            emitter.send(SseEmitter.event()
+                    .id(emitterId)
+                    .name(data.getNotificationType().getValue())
+                    .data(data));
+        } catch (IOException | java.io.IOException ex) {
+            emitterRepository.deleteById(emitterId);
+        }
+    }
+
+
+    /**
+     *
+     * 일반 데이터 전송용
+     * @param emitter
+     * @param emitterId
+     * @param data
+     */
+    private void send(SseEmitter emitter, String emitterId, Object data) {
+        try {
+            emitter.send(SseEmitter.event()
+                    .id(emitterId)
+                    .data(data));
+        } catch (IOException | java.io.IOException e) {
+            emitterRepository.deleteById(emitterId);
+            log.error("알림 전송에 실패했습니다", e);
+        }
     }
 }
