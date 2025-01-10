31a1f9669986638ad98aec7f1f1a337d14ac3a55
minthug <jkisimmortal4@gmail.com>
2025-01-10 16:20:08 +0900

    Add custom queries for fetching UserCoupon data

--- backend/src/main/java/SniffStep/repository/UserCouponRepository.java
+++ backend/src/main/java/SniffStep/repository/UserCouponRepository.java
@@ -4,6 +4,7 @@ import SniffStep.entity.Coupon;
 import SniffStep.entity.Member;
 import SniffStep.entity.UserCoupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
@@ -12,10 +13,13 @@ import java.util.Optional;
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
 
+    @Query("SELECT uc from UserCoupon uc JOIN fetch uc.coupon c WHERE uc.id = :id")
     Optional<UserCoupon> findByIdWithCoupon(Long id);
 
     boolean existsByMemberAndCoupon(Member member, Coupon coupon);
 
+    @Query("SELECT uc from UserCoupon uc JOIN FETCH uc.coupon c "
+    + "WHERE uc.member = :member AND uc.isUsed = :isUsed AND c.endAt >= :currentDate")
     List<UserCoupon> findByMemberAndIsUsedAndCouponEndAtAfter(@Param("member") Member member,
                                                               @Param("isUsed") boolean isUsed,
                                                               @Param("currentDate")LocalDate localDate);
